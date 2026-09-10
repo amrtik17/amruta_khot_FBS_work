85e6e401bd6b366407bf0d01225d03c69f3b34d0
amrtik17 <26amrutakhot@gmail.com>
2026-09-10 09:47:59 +0530

    Delete question_2.py
    file unload 2 time

--- question_2.py
+++
@@ -1,8 +0,0 @@
-#Write a program to input any alphabet and check whether it is vowel or consonant.
-
-char = input("Enter an alphabet: ")
-
-if char in "aeiouAEIOU":
-    print("It is a vowel")
-else:
-    print("It is a consonant")
